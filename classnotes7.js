0c9d70bd5c8d570566bf4fdac8ca8fa1ee5c1cb3
Batlijo <batlijoselevitz@gmail.com>
2017-10-23 14:34:01 -0700

    the beginning

---
+++ classnotes7.js
@@ -0,0 +1,34 @@
+/*
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Sign-Up Today! </title>
+	FIREBASE
+	<script src="https://www.gstatic.com/firebase.js"></script>
+	JQUERY
+	<script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+	BOOTSTRAP
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="
+	sha384=1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjzlplegxhjVME1fgjWPGmkszs7" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container"
+	<div class="jumbotron">
+		<h1 class="text-center">Sign-Up Today!</h1>
+	</div>
+
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">New Member Sign-Up!</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html?
+
